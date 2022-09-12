3763a39965530c32f1fa60c2b677f28371bf203d
hasnaabelb <belabsilihasnaa@gmail.com>
2022-09-12 15:32:32 +0100

    fonction

---
+++ Desktop/FONCTIONS/FONCTION.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float cube (float N){
+float C;
+C=N*N*N;
+return C ;
+}
+int main ()
+{
+    float N ;
+    printf("Veuillez entrer la valeur de N : ");
+    scanf("%f",&N);
+    printf("le cube de %.2f est : %.2f\n",N,cube(N));
+    return 0;
+}
